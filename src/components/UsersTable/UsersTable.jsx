0291a469a7bbb88d7d28c2237afc032368ed3927
Meep <mwraich28@gmail.com>
2023-07-31 17:48:20 -0400

    Setup Drag and Dropping functionality

--- src/components/UsersTable/UsersTable.jsx
+++ src/components/UsersTable/UsersTable.jsx
@@ -1,6 +1,8 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useTable } from 'react-table';
 import { getFullName, getDaysSinceRegistered } from '../../helpers/users';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './UsersTable.css';
 
 const UsersTable = ({ users }) => {
@@ -10,41 +12,98 @@ const UsersTable = ({ users }) => {
 			{
 				Header: 'ID',
 				accessor: 'id',
+				id: 'id',
 			},
 			{
 				Header: 'First Name',
 				accessor: 'firstName',
+				id: 'firstName',
 			},
 			{
 				Header: 'FullName',
 				accessor: (user) => getFullName(user),
+				id: 'FullName',
 			},
 			{
 				Header: 'Last Name',
 				accessor: 'lastName',
+				id: 'lastName',
 			},
 			{
 				Header: 'Email',
 				accessor: 'email',
+				id: 'email',
 			},
 			{
 				Header: 'City',
 				accessor: 'city',
+				id: 'city',
 			},
 			{
 				Header: 'Registered Date',
 				accessor: 'registeredDate',
+				id: 'registeredDate',
 			},
 			{
 				Header: 'DSR',
 				accessor: (user) => getDaysSinceRegistered(user),
+				id: 'daysSinceRegistered',
 			},
 		],
 		[],
 	);
+
+	const [columnOrder, setColumnOrder] = useState(columns.map((col) => col.id));
+
+	const orderedColumns = useMemo(() => {
+		return columnOrder.map((id) => columns.find((col) => col.id === id));
+	}, [columnOrder]);
+
+	const reorderColumn = (draggedColumnId, dropColumnId, columnOrder) => {
+		// get the index of the dragged and drop columns
+		const draggedColumnIndex = columnOrder.indexOf(draggedColumnId);
+		const dropColumnIndex = columnOrder.indexOf(dropColumnId);
+
+		// Remove the dragged column from the order
+		const [draggedColumn] = columnOrder.splice(draggedColumnIndex, 1);
+
+		// Add it back to it's new dropped position
+		columnOrder.splice(dropColumnIndex, 0, draggedColumn);
+		return [...columnOrder];
+	};
+
+	// Create a DraggableHeader
+	const DraggableHeader = ({ column }) => {
+		const [{ opacity }, drag] = useDrag(() => ({
+			type: 'column',
+			item: column,
+			collect: (monitor) => ({
+				opacity: monitor.isDragging() ? 0.5 : 1,
+			}),
+		}));
+
+		const [, dropRef] = useDrop({
+			accept: 'column',
+			drop: (draggedColumn) => {
+				const newColumnOrder = reorderColumn(
+					draggedColumn.id,
+					column.id,
+					columnOrder,
+				);
+				setColumnOrder(newColumnOrder);
+			},
+		});
+
+		return (
+			<th ref={dropRef} {...column.getHeaderProps()} style={{ opacity }}>
+				{column.render('Header')}
+				<button ref={drag}>X</button>
+			</th>
+		);
+	};
 	// Create an instance of the table
 	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-		useTable({ columns, data: users });
+		useTable({ columns: orderedColumns, data: users });
 
 	return (
 		<table {...getTableProps()}>
@@ -52,7 +111,7 @@ const UsersTable = ({ users }) => {
 				{headerGroups.map((headerGroup) => (
 					<tr {...headerGroup.getHeaderGroupProps()}>
 						{headerGroup.headers.map((column) => (
-							<th {...column.getHeaderProps()}>{column.render('Header')}</th>
+							<DraggableHeader key={column.id} column={column} />
 						))}
 					</tr>
 				))}
@@ -61,9 +120,11 @@ const UsersTable = ({ users }) => {
 				{rows.map((row) => {
 					prepareRow(row);
 					return (
-						<tr {...row.getRowProps()}>
+						<tr key={row.id} {...row.getRowProps()}>
 							{row.cells.map((cell) => (
-								<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+								<td key={cell.id} {...cell.getCellProps()}>
+									{cell.render('Cell')}
+								</td>
 							))}
 						</tr>
 					);
@@ -73,4 +134,11 @@ const UsersTable = ({ users }) => {
 	);
 };
 
-export default UsersTable;
+function DnDUsersTable({ ...props }) {
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<UsersTable {...props} />
+		</DndProvider>
+	);
+}
+export default DnDUsersTable;
